becd0c11a14d57facb910e95de9dce91dec2fd7d
Jon Miguel Gutierrez <jonmiguel.gutierrez@cit.edu>
2023-10-01 20:10:19 +0800

    second commit

--- src/hellogit/HelloGit.java
+++ src/hellogit/HelloGit.java
@@ -14,7 +14,11 @@ public class HelloGit {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        secondCommit();
+    }
+    
+    public static void secondCommit(){
+        System.out.println("Hello git");
     }
     
 }
